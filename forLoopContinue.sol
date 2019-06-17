4c02b41cbc31a5e7e50daede7cc77d5e68d48345
mirzafurqaan <48507334+mirzafurqaan@users.noreply.github.com>
2019-06-17 23:31:56 +0530

    Create forLoopContinue.sol

---
+++ forLoopContinue.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.19;
+
+contract forLoopContinue{
+    mapping (uint => uint) blockNumber;
+    uint counter;
+    
+    event uintNumber(uint);
+    
+    function SetNumber() public {
+        blockNumber[counter++] = block.number;
+    }
+    
+    function getNumbers() public {
+        
+        for (uint i=0; i< counter; i++){
+            if (i > 5)
+            {continue;}
+            uintNumber(blockNumber[i]);
+        }
+    }
+}

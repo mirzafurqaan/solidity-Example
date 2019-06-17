5ea42a941ab7af6ca904aba380d94f0ef2af2e71
mirzafurqaan <48507334+mirzafurqaan@users.noreply.github.com>
2019-06-17 23:23:14 +0530

    Create forLoop.sol

---
+++ forLoop.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.19;
+
+contract forLoop{
+    mapping (uint => uint) blockNumber;
+    uint counter;
+    event uintNumber(uint);
+    
+    function SetNumber() public {
+        blockNumber[counter++] = block.number;
+    }
+    
+    function getNumbers() public {
+        for (uint i=0; i< counter; i++){
+            uintNumber(blockNumber[i]);
+        }
+    }
+}
